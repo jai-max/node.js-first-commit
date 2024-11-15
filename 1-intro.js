5146bb1e75b37e0f93379b63f7f4aaaa3ccc362c
AryanCoder <kadyanaryan744@gmail.com>
2024-11-15 20:55:37 +0530

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 23
+
+if(amount < 15){
+    console.log('small number')
+
+}else{
+    console.log('large number')
+}
+
+console.log(`Hey its my first node app!!!`)
---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// GlOBALS - NO WINDOW !!!!
+
+// _dirname - path to current directory 
+// _filename - file name
+// require - function to use modulues (CommonJS)
+// module - info about current module (file)
+// process - info about env where the program is being executed
+
+console.log(__dirname)
+setInterval(()=>{
+    console.log('hello world')
+}, 2000)
